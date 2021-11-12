d01e2dbd26de0c8782e77b4c7c1fe81f4a7cf42c
Michael Hearn <michael.hearn@fizeekz.com>
2021-11-12 08:40:43 +0000

    Cleaned up code

--- src/app/currency-converter.service.ts
+++ src/app/currency-converter.service.ts
@@ -4,27 +4,16 @@ import { Observable } from 'rxjs';
 import { CurrencyRequest } from './models/currency-request';
 import { CurrencyResponse } from './models/currency-response';
 import { environment } from '../environments/environment';
-//import 'rxjs/add/observable/empty';
 
 @Injectable()
 export class CurrencyConverterService {
-    //currencyRequest: CurrencyRequest;
+    
     baseUrl = environment.currencyConverterApiUrl;
 
     constructor(private http: HttpClient) {
-        /*this.currencyRequest = {
-            to: '',
-            from: '',
-            amount: 0
-        } */
     }
 
     convertCurrency(currencyRequest: CurrencyRequest): Observable<CurrencyResponse> {
-        //this.currencyRequest = currencyRequest;
-
-        //return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
-
-        //CurrencyResponse
 
         let url = this.baseUrl + 'currencies';
 
@@ -35,24 +24,5 @@ export class CurrencyConverterService {
                                                         'Content-Type': 'application/json'
                                                     }
                                                 });
-          /*.pipe(
-            catchError(this.handleError('addHero', hero))
-          );*/
     }
-
-    /*private handleError(error: HttpErrorResponse) {
-        if (error.status === 0) {
-          // A client-side or network error occurred. Handle it accordingly.
-          console.error('An error occurred:', error.error);
-        } else {
-          // The backend returned an unsuccessful response code.
-          // The response body may contain clues as to what went wrong.
-          console.error(
-            `Backend returned code ${error.status}, body was: `, error.error);
-        }
-        // Return an observable with a user-facing error message.
-        //return throwError('Something bad happened; please try again later.');
-        return Observable.empty<CurrencyResponse>();
-      }*/
-
   }
